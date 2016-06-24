1983c4b3ea331cdc734e71bed08d4887d4ce266f
Dayan Lazare <dlazare@ms3-inc.com>
2016-06-23 21:39:29 -0400

    Step1

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,18 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!;
+    @IBOutlet weak var mealNameLabel: UILabel!;
+    @IBOutlet weak var photoImageView: UIImageView!;
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad();
+        
+        // Handle the text field’s user input through delegate callbacks.
+        self.nameTextField.delegate = self;
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.nameTextField.resignFirstResponder();
+        return true;
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        self.mealNameLabel.text = textField.text;
+    }
 
-
+    // MARK: Actions
+    
+    @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
+        
+    }
+    
+    @IBAction func setDefaultLabelText(sender: UIButton) {
+        self.mealNameLabel.text = "Default Text";
+    }
 }
 
